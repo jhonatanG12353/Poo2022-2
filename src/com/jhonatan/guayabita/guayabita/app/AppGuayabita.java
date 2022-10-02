9cde75825214ef1f482f02a87935760e3d526e8a
Jhonatan Gomez <jhonatan12353@gmail.com>
2022-10-01 21:23:21 -0500

    .

--- src/com/jhonatan/guayabita/guayabita/app/AppGuayabita.java
+++ src/com/jhonatan/guayabita/guayabita/app/AppGuayabita.java
@@ -1,5 +1,6 @@
 package com.jhonatan.guayabita.guayabita.app;
 
+import com.jhonatan.guayabita.guayabita.dominio.Dado;
 import com.jhonatan.guayabita.guayabita.dominio.Jugadores;
 
 import javax.swing.*;
@@ -7,17 +8,26 @@ import java.util.Arrays;
 
 public class AppGuayabita {
     private static final ImageIcon ICONO = new ImageIcon(AppGuayabita.class.getResource("guayabita.png"));
+    public static final ImageIcon DADO1 = new ImageIcon(AppGuayabita.class.getResource("dado1.png"));
+    private static final ImageIcon DADO2 = new ImageIcon(AppGuayabita.class.getResource("dado2.png"));
+    private static final ImageIcon DADO3 = new ImageIcon(AppGuayabita.class.getResource("dado3.png"));
+    private static final ImageIcon DADO4 = new ImageIcon(AppGuayabita.class.getResource("dado4.png"));
+    private static final ImageIcon DADO5 = new ImageIcon(AppGuayabita.class.getResource("dado5.png"));
+    private static final ImageIcon DADO6 = new ImageIcon(AppGuayabita.class.getResource("dado6.png"));
+
     private static final int JUGAR = 0;
     private static final int PUNTAJE = 2;
     private static final int INSTRUCCIONES = 1;
+    private static final int SALIR= 3;
 
     public static void main(String[] args) {
         Jugadores jugadores = new Jugadores();
-
-        while(true) {
+        Dado dado = new Dado();
+        boolean op= true;
+        while(op==true) {
             int opcion = JOptionPane.showOptionDialog(null, "Bienvenido al Juego de la Guayabita", "Juego de la Guayabita",
                     0, 0, ICONO,
-                    Arrays.asList("Jugar", "Instrucciones", "Puntaje").toArray(), null);
+                    Arrays.asList("Jugar", "Instrucciones", "Puntaje","Salir").toArray(), null);
 
             switch(opcion){
                 case JUGAR:
@@ -26,16 +36,30 @@ public class AppGuayabita {
                          nombreJugador = (String) JOptionPane.showInputDialog(null,"Ingrese el Nombre del Jugador 1.","ingrese nombre",JOptionPane.PLAIN_MESSAGE,ICONO,null,null);
                          jugadores.setJugador1(nombreJugador);
                      }
+                    if (jugadores.getJugador1() != null){
+                        double dinerJugador1;
+                        dinerJugador1 = Double.parseDouble((String) JOptionPane.showInputDialog(null,"Ingrese el Dinero del Jugador 1.","ingrese Dinero de Juego",JOptionPane.PLAIN_MESSAGE,ICONO,null,null));
+                        jugadores.setDineroJugador1(dinerJugador1);
+                    }
                     if (jugadores.getJugador2() == null){
                         String nombreJugador;
                         nombreJugador = (String) JOptionPane.showInputDialog(null,"Ingrese el Nombre del Jugador 2.","ingrese nombre",JOptionPane.PLAIN_MESSAGE,ICONO,null,null);
                         jugadores.setJugador2(nombreJugador);
+                    }
+                    if (jugadores.getJugador2() != null){
+                        double dinerJugador2;
+                        dinerJugador2 = Double.parseDouble((String) JOptionPane.showInputDialog(null,"Ingrese el Dinero del Jugador 1.","ingrese Dinero de Juego",JOptionPane.PLAIN_MESSAGE,ICONO,null,null));
+                        jugadores.setDineroJugador2(dinerJugador2);
                     }
                     break;
                 case INSTRUCCIONES:
                     JOptionPane.showMessageDialog(null,"instrucciones guayabita","Juego de la guayabita",JOptionPane.PLAIN_MESSAGE,ICONO);
                     break;
                 case PUNTAJE:
+                    break;
+                case SALIR :
+                    JOptionPane.showMessageDialog(null,"Gracias por Jugar, Hasta la Proxima!!!","Salida del Juego de la guayabita",JOptionPane.PLAIN_MESSAGE,ICONO);
+                    op=false;
                     break;
             }
         }
--- src/com/jhonatan/guayabita/guayabita/dominio/Jugadores.java
+++ src/com/jhonatan/guayabita/guayabita/dominio/Jugadores.java
@@ -3,6 +3,8 @@ package com.jhonatan.guayabita.guayabita.dominio;
 public class Jugadores {
     private String jugador1;
     private String jugador2;
+    private double dineroJugador1;
+    private double dineroJugador2;
 
     public String getJugador1() {
         return jugador1;
@@ -18,5 +20,21 @@ public class Jugadores {
 
     public void setJugador2(String jugador2) {
         this.jugador2 = jugador2;
+    }
+
+    public double getDineroJugador1() {
+        return dineroJugador1;
+    }
+
+    public double getDineroJugador2() {
+        return dineroJugador2;
+    }
+
+    public void setDineroJugador1(double dineroJugador1) {
+        this.dineroJugador1 = dineroJugador1;
+    }
+
+    public void setDineroJugador2(double dineroJugador2) {
+        this.dineroJugador2 = dineroJugador2;
     }
 }
